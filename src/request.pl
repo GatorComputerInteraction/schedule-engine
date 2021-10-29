fd1f315b28910439ab1714a9f45e257819765e21
Christopher Miller <admin@mctherealm.net>
2021-10-29 12:43:40 -0400

    Updated with new backend changes

--- src/request.pl
+++ src/request.pl
@@ -19,14 +19,14 @@ get_json_endpoint(Path, Result) :-
   close(In).
 
 % Ensures course id and instance match.
-find_instance_by_course_id(CourseID, _{instanceId: _, semester: _, year: _, courseId: CourseID, slotId: _, courseModel: _, timeslotModel: _}).
+find_instance_by_course_id(CourseID, _{instanceId: _, semester: _, year: _, courseId: CourseID, slotId: _}).
 
-find_degree_course_by_id(DegreeId, _{ degreeId: DegreeId, courseId: _, degreeModel: _, courseModel: _ }).
+find_degree_course_by_id(DegreeId, _{ degreeId: DegreeId, courseId: _, requirementType: _ }).
 
-select_course_id(_{ degreeId: _, courseId: Id, degreeModel: _, courseModel: _ }, Id).
+select_course_id(_{ degreeId: _, courseId: Id, requirementType: _ }, Id).
 
 % Extracts slot id from instance id json
-slot_id_from_instance(_{courseId:_,courseModel:_,instanceId:_,semester:_,slotId: ID,timeslotModel:_,year:_}, ID).
+slot_id_from_instance(_{courseId:_,instanceId:_,semester:_,slotId: ID,year:_}, ID).
 
 % Builds period day pairs
 % TODO: Remove single item list for period, needs to be modified in schedule.pl
@@ -43,9 +43,9 @@ build_day_period_list(Days, [PeriodNumber|B], List) :-
   append(Pairs, ListTail, List).
 
 % Joins timeslot with the instance id, converting json values into usable atoms and data structures.
-timeslot_from_instance_id(SlotIds, _{slotId: SlotId, day: _, periodId1: _, periodId2: _, periodId3: _, periodMode1l: _, periodModel2: _, periodModel3: _}, []) :-
+timeslot_from_instance_id(SlotIds, _{slotId: SlotId, day: _, periodId1: _, periodId2: _, periodId3: _}, []) :-
   \+ member(SlotId, SlotIds).
-timeslot_from_instance_id(SlotIds, _{slotId: SlotId, day: DayString, periodId1: PeriodOne, periodId2: PeriodTwo, periodId3: PeriodThree, periodMode1l: _, periodModel2: _, periodModel3: _}, MeetingList) :-
+timeslot_from_instance_id(SlotIds, _{slotId: SlotId, day: DayString, periodId1: PeriodOne, periodId2: PeriodTwo, periodId3: PeriodThree}, MeetingList) :-
   member(SlotId, SlotIds),
   day_string_to_atom(DayString, Days),
   build_day_period_list(Days, [PeriodOne, PeriodTwo, PeriodThree], MeetingList).
@@ -53,7 +53,7 @@ timeslot_from_instance_id(SlotIds, _{slotId: SlotId, day: DayString, periodId1: 
 % Combines a course with it's instances and time slots.
 join_course_data(CourseInstances, TimeSlots, _{courseId: ID, courseName: Name, credits: Credits}, (ID, Semester, Year, Name, Credits, MeetingList)) :-
   include(find_instance_by_course_id(ID), CourseInstances, InstancesForID),
-  InstancesForID = [_{courseId:_,courseModel:_,instanceId:_,semester: SemesterString ,slotId: _,timeslotModel:_,year: Year}|_],
+  InstancesForID = [_{courseId:_,instanceId:_,semester: SemesterString ,slotId: _,year: Year}|_],
   termFromString(SemesterString, Semester),
   maplist(slot_id_from_instance, InstancesForID, SlotIds),
   maplist(timeslot_from_instance_id(SlotIds), TimeSlots, MeetingListNested),
@@ -66,9 +66,9 @@ course_empty(CourseInstances, _{courseId: ID, courseName: _, credits: _}) :-
 
 % Returns needed JSON objects from the endpoint.
 get_courses_endpoints(Courses, CourseInstances, TimeSlots) :-
-  get_json_endpoint('/course', Courses),
-  get_json_endpoint('/courseinstance', CourseInstances),
-  get_json_endpoint('/timeslot', TimeSlots).
+  get_json_endpoint('/Course', Courses),
+  get_json_endpoint('/Courseinstance', CourseInstances),
+  get_json_endpoint('/Timeslot', TimeSlots).
 
 get_required_courses(DegreeId, CourseIdList) :-
   get_json_endpoint('/DegreeCourse', DegreeCourseList),
